cc35a9e1bb333636a9851260842df645d8dfe95b
roundrobine <roundrobine@gmail.com>
2018-02-15 03:29:57 +0100

    add an Author controller and create author and books thymeleaf templates

---
+++ src/main/java/com/roundrobine/spring5webapp/controllers/Author.java
@@ -0,0 +1,24 @@
+package com.roundrobine.spring5webapp.controllers;
+
+import com.roundrobine.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Author {
+
+    private AuthorRepository authorRepository;
+
+    public Author(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors";
+    }
+}
